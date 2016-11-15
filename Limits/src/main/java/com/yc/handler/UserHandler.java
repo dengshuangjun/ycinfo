52e5a8c4b4dc8876fcd3b36485793a17719b14c9
shuang <862454410@qq.com>
2016-11-15 22:01:21 +0800

    登陆写到了控制层，使用了过滤器避免直接访问page下的jsp页面，使用拦截器拦截除了登陆以外的其他任何非jsp请求

---
+++ Limits/src/main/java/com/yc/handler/UserHandler.java
@@ -0,0 +1,27 @@
+package com.yc.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.yc.entity.User;
+import com.yc.utils.SessionAttributeKey;
+
+@Controller
+@RequestMapping("/front")
+@SessionAttributes("user")
+public class UserHandler extends BasicHandler{
+	
+	   @RequestMapping("login.do")
+		public String Login(User user,Model model){
+		   if(user.getUid()!=0){
+			   model.addAttribute(SessionAttributeKey.LOGIN_ADMIN, user);
+			   System.out.println(user);
+				return "page/list";
+		   }else{
+			   return "index";
+		   }
+		
+		}
+}
---
+++ Limits/src/main/java/com/yc/interceptors/LoginInterceptors.java
@@ -0,0 +1,35 @@
+package com.yc.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.yc.utils.SessionAttributeKey;
+
+public class LoginInterceptors implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object arg2) throws Exception {
+		if(request.getSession().getAttribute(SessionAttributeKey.LOGIN_ADMIN)!=null){
+			String path=request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
+			response.getWriter().print("<script>location.href='"+path+"/page/list.jsp'</script>");
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+
+	}
+}
